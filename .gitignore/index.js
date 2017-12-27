18fd4cc26aeebf4c2a1efd82a2249c24a61351ba
DerpyFoxMan <34697861+DerpyFoxMan@users.noreply.github.com>
2017-12-27 16:59:45 -0500

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -19,20 +19,4 @@ bot.on('message', message => {
         message.channel.sendMessage("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BTC,USD");
         console.log("Commande BTC-USD effectuée");
     }
-    
-    if (message.content === prefix + "pb") {
-        function BTC() {
-            return new Promise((resolve) => {
-                // send a request to blockchain
-                request('https://blockchain.info/de/ticker', (error, reponse, body) => {
-                    // parse the json answer and get the current bitcoin value
-                    const data = JSON.parse(body);
-                    value = (parseInt(data.THB.buy, 10) + parseInt(data.THB.sell, 10)) / 2;
-                    resolve(value);
-                });
-            });
-        }
-        BTC().then(val => message.channel.sendMessage(val)
-    }
-    
 });//fin de message
